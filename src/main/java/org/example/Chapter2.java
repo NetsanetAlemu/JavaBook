ef4d3eb7b666a3bfb9d36568fa0ea0604caa9827
NetsanetAlemu <netsialemu1907@gmail.com>
2023-06-10 10:24:20 +0300

    Exercise 2.6: Sum the digits in an integer

--- src/main/java/org/example/Chapter2.java
+++ src/main/java/org/example/Chapter2.java
@@ -1,8 +1,8 @@
 package org.example;
-
+import java.util.Scanner;
 public class Chapter2 {
     public static void main(String[] args) {
-      // Current time using UNIX epoch
+      // Current time using UNIX epoch - january 1, 1970
         long totalMilliSec = System.nanoTime();
         long totalSeconds = totalMilliSec / 1000000000;
         long currentSeconds = totalSeconds % 60;
@@ -13,6 +13,18 @@ public class Chapter2 {
 
         System.out.println("Current time is " + currentHours + ":" +
                 currentMinutes + ":" + currentSeconds + " GMT");
+        // Sum the digits in an integer between 0 and 1000
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number between 0 and 1000");
+        int number = input.nextInt();
+        int sum = 0, digit = 100;
+        for(int i = 0; i <= 2; i++){
+            sum += number / digit;
+            number = number % digit;
+            digit /= 10;
+        }
+        System.out.println("Sum: " + sum);
+
 
 
     }
