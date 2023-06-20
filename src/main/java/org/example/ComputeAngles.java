9e26825434b51ecd8f0df0b61f8138b6d33b0ae2
NetsanetAlemu <netsialemu1907@gmail.com>
2023-06-20 05:28:47 +0300

    Compute Angles

---
+++ src/main/java/org/example/ComputeAngles.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ComputeAngles {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double x1, y1, x2, y2, x3, y3, a, b, c, A, B, C;
+
+        System.out.println("Enter the co-ordinates of the three vertices of the triangle");
+        x1 = input.nextDouble();
+        y1 = input.nextDouble();
+        x2 = input.nextDouble();
+        y2 = input.nextDouble();
+        x3 = input.nextDouble();
+        y3 = input.nextDouble();
+
+        a = Math.sqrt((x2 - x3) * (x2 - x3) + (y2 - y3) * (y2 - y3));
+        b = Math.sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
+        c = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+
+        A = Math.toDegrees(Math.acos((a * a) - (b * b) - (c * c)) / (-2 * b * c));
+        B = Math.toDegrees(Math.acos((b * b) - (a * a) - (c * c)) / (-2 * a * c));
+        C = Math.toDegrees(Math.acos((c * c) - (b * b) - (a * a)) / (-2 * a * b));
+
+        System.out.println("Angle A: " + Math.round(A * 100) / 100.0);
+        System.out.println("Angle B: " + Math.round(B * 100) / 100.0);
+        System.out.println("Angle C: " + Math.round(C * 100) / 100.0);
+
+
+
+
+        input.close();
+    }
+}
