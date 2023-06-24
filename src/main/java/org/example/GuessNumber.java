d110f1b4836bd4d66f088fd4f9469ab282f2b226
NetsanetAlemu <netsialemu1907@gmail.com>
2023-06-24 04:34:08 +0300

    Random variable declaratioin corrected.

--- src/main/java/org/example/GuessNumber.java
+++ src/main/java/org/example/GuessNumber.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class GuessNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int number = (int)Math.random() * 101;
+        int number = (int)(Math.random() * 101);
         int guess = -1;
         while (guess != number) {
             System.out.println("Enter your guess: ");
