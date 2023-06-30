8c511089f7abb2b3756141b6d025b84f52d74532
NetsanetAlemu <netsialemu1907@gmail.com>
2023-06-30 10:12:35 +0300

    Future Tuition

---
+++ src/main/java/org/example/FutureTuition.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class FutureTuition {
+    public static void main(String[] args) {
+        double tuition = 10_000;
+        int year = 0;
+        while (tuition < 20_000) {
+            tuition = tuition * 1.07;
+            year++;
+        }
+        System.out.println("Tuition will be doubled in " + year);
+        System.out.printf("Tuition will be $%.2f in %1d years", tuition, year);
+    }
+}
